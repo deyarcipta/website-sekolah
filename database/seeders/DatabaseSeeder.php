1342402bc53c0ad1d065d6c155b879fee0078460
deyarcipta <deyar.cipta@gmail.com>
2026-01-25 15:46:21 +0700

    perbaikan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(AdminSeeder::class);
         $this->call(WebsiteSettingSeeder::class);
+        $this->call(WebsiteStatisticsSeeder::class);
+        $this->call(KontaSeeder::class);
     }
 }
